d976f7a3f0473c9ca98f38066fc7d8fb31d275a0
Juan Carrasquer <juancarrasquer@gmail.com>
2024-03-26 11:14:39 +0100

    Corregido error de aliniación de texto en la tarea

--- src/components/Task.js
+++ src/components/Task.js
@@ -10,27 +10,29 @@ export default function Task(props)
         <Grid.Column width={8} className='task-container'>
              <Segment>
             {task.categoryName && (
-                <Label color='teal' ribbon='right'>
+                <Label color='teal' ribbon='right' className='no-center'>
                     {task.categoryName}
                 </Label>
                 
             )}
 
-            <Header as="h3" className='header-task'>
-                 {task.taskName}   
-            </Header>
-            <Header as="h5" className='header-task'>
-                 {task.idtask}   
-            </Header>
+            <div className="content-wrapper">
+                <Header as="h3" className='header-task'>
+                {task.taskName}   
+                </Header>
+                <Header as="h5" className='header-task'>
+                {task.idtask}   
+                </Header>
 
-            <Grid centered columns={2}>
-                <Grid.Column>
-                    <Button color='red' onClick={()=>deleteTask(task.idtask)}>
-                        <Icon name='remove circle'/>
-                        ELIMINAR
+                <Grid centered columns={2}>
+                <Grid.Column textAlign='center'>
+                    <Button color='red' onClick={() => deleteTask(task.idtask)}>
+                    <Icon name='remove circle'/>
+                    ELIMINAR
                     </Button>
                 </Grid.Column>
-            </Grid>
+                </Grid>
+            </div>
             </Segment>
         </Grid.Column>
         
